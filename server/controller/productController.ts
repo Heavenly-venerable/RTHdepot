e1c04c8fcb85e48c2caa5f55e37b17cf6e00bc9f
Heavenly-venerable <ricrichar94@gmail.com>
2025-06-27 08:54:33 +0700

    feat(product): add POST endpoint to create a new product

--- server/controller/productController.ts
+++ server/controller/productController.ts
@@ -1,4 +1,7 @@
+import { products } from "../data/products";
 import { Product } from "../models/product.model";
+import { ProductSchema } from "../schemas/product";
+import { ProductInterface } from "../types/product";
 
 export const ProductController = {
   async getAllProducts() {
@@ -9,5 +12,28 @@ export const ProductController = {
     }
 
     return products
-  }
+  },
+  async createInvoice(data: Omit<ProductInterface, "id">) {
+    const parsed = ProductSchema.omit({ id: true }).safeParse(data)
+
+    if (!parsed.success) {
+      return {
+        success: false,
+        message: "Data tidak valid",
+        errors: parsed.error.flatten()
+      }
+    }
+
+    const newProduct: ProductInterface = {
+      id: String(products.length + 1),
+      ...parsed.data,
+    }
+
+    Product.create(newProduct)
+
+    return {
+      success: true,
+      message: "Product berhasil ditambahkan",
+    }
+  },
 }
--- server/models/product.model.ts
+++ server/models/product.model.ts
@@ -1,7 +1,11 @@
 import { products } from "../data/products"
+import { ProductInterface } from "../types/product"
 
 export const Product = {
   findAll() {
     return products
-  }
+  },
+  create(data: ProductInterface) {
+    products.push(data)
+  },
 }
