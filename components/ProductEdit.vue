d6cf17c37542268ccb50fa021d1d5cca6dd993ca
Heavenly-venerable <ricrichar94@gmail.com>
2025-06-28 16:08:39 +0700

    feat(product): complete product update feature with API and UI integration

---
+++ components/ProductEdit.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+const props = defineProps<{
+  product: any | null,
+  visible: boolean
+}>()
+
+const emit = defineEmits(["update:visible"])
+
+const route = useRoute()
+
+const form = reactive({
+  name: "",
+  price: 0,
+  stock: 0
+})
+
+const errors = reactive({
+  name: "",
+  price: "",
+  stock: ""
+})
+
+function validate() {
+  let valid = true
+  errors.name = form.name ? "" : "Nama ikan wajib diisi"
+  errors.price = form.price > 0 ? "" : "Harga harus lebih dari 0"
+  errors.stock = form.stock >= 0 ? "" : "Stok tidak boleh negatif"
+
+  if (errors.name || errors.price || errors.stock) valid = false
+  return valid
+}
+
+async function onFormSubmit() {
+  if (!validate()) return
+
+  const id = props.product?.id || route.params.id
+
+  await $fetch(`/api/products/${id}`, {
+    method: "PATCH",
+    body: form
+  })
+
+  emit("update:visible", !props.visible)
+}
+
+onMounted(() => {
+  form.name = props.product?.name ?? ""
+  form.price = props.product?.price ?? 0
+  form.stock = props.product?.stock ?? 0
+})
+</script>
+
+<template>
+  <form @submit.prevent="onFormSubmit()" class="flex flex-col pb-6 gap-4 w-full md:w-56">
+    <div class="flex flex-col gap-2">
+      <label class="text-sm" for="name">Nama Ikan</label>
+      <InputText v-model="form.name" id="name" type="text" placeholder="Nama Ikan" />
+      <small v-show="errors.name" class="text-red-600">{{ errors.name }}</small>
+    </div>
+
+    <div class="flex flex-col gap-2">
+      <label class="text-sm" for="price">Harga / KG</label>
+      <InputNumber v-model="form.price" id="price" mode="currency" currency="IDR" locale="id-ID" :minFractionDigits="0"
+        :useGrouping="true" :min="1000" fluid />
+      <small v-show="errors.price" class="text-red-600">{{ errors.price }}</small>
+    </div>
+
+    <div class="flex flex-col gap-2">
+      <label class="text-sm" for="stock">Stock (KG)</label>
+      <InputNumber v-model="form.stock" id="stock" :min="0" fluid />
+      <small v-show="errors.stock" class="text-red-600">{{ errors.stock }}</small>
+    </div>
+
+    <Button fluid type="submit" severity="primary" label="Submit" />
+  </form>
+</template>
