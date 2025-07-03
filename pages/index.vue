5084320f6c98d93af03889b7745b1b74cc7a1130
Heavenly-venerable <ricrichar94@gmail.com>
2025-07-03 20:45:32 +0700

    feat: add summary card for totalSaleToday and totalIncomeToday

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,17 @@ const { data: invoices } = await useFetch("/api/invoices")
 
 const todayInvoices = computed(() => invoices.value?.filter((invoice) => isToday(invoice.createAt)) ?? [])
 const totalInvoiceToday = computed(() => todayInvoices.value.length)
-const totalPurchaseToday = computed(() => todayInvoices.value.reduce((sum, invoice) => sum + invoice.total, 0))
+const totalPurchaseToday = computed(() => todayInvoices.value.filter((invoice) => invoice.type === 'purchase').reduce((sum, invoice) => sum + invoice.total, 0))
+const totalSaleToday = computed(() => todayInvoices.value.filter((invoice) => invoice.type === 'sale').reduce((sum, invoice) => sum + invoice.total, 0))
+const totalIncomeToday = computed(() => totalSaleToday.value - totalPurchaseToday.value)
 </script>
 
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
     <SummaryCard title="Total Invoice" :value="totalInvoiceToday" icon="pi pi-users" />
     <SummaryCard title="Total Pembelian" :value="formatPrice(totalPurchaseToday)" icon="pi pi-shopping-cart" />
+    <SummaryCard title="Total Penjualan" :value="formatPrice(totalSaleToday)" icon="pi pi-chart-line" />
+    <SummaryCard title="Pendapatan Hari Ini" :value="formatPrice(totalIncomeToday)" icon="pi pi-wallet" />
   </div>
 </template>
 
